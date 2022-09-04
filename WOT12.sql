d186bd88c1f0f00bde1636ce089809bfab124cd9
Suvek Thange <98460649+SuvekT@users.noreply.github.com>
2022-09-04 23:24:55 +0530

    Update WOT12.sql
    https://www.hackerrank.com/challenges/weather-observation-station-12/copy-from/286925310?isFullScreen=true

--- WOT12.sql
+++ WOT12.sql
@@ -1,3 +1,2 @@
-problem link - https://www.hackerrank.com/challenges/weather-observation-station-12/copy-from/286925310?isFullScreen=true
-SOL -
+
 SELECT DISTINCT CITY FROM STATION WHERE LOWER(SUBSTR(CITY,1,1)) NOT IN ('a','e','i','o','u') AND LOWER(SUBSTR(CITY,LENGTH(CITY),1)) NOT IN ('a','e','i','o','u');
